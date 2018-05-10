f2d2aac4d60de47518300f9593ddca8d1d78e84a
Michael Boroski <mjboroski@gmail.com>
2018-05-10 21:46:44 +0000

    expanded sites.rb

---
+++ lib/sites.rb
@@ -0,0 +1,24 @@
+module Sites
+
+  module COINDESK
+    def scrape
+
+      # gets articles, top_article
+    end
+  end
+
+  module COINTELEGRAPH
+    def scrape
+
+      # gets articles, top_article
+    end
+  end
+
+  module CCN
+    def scrape
+
+      # gets articles, top_article
+    end
+  end
+
+end
